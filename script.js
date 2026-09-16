7c66763e80ab9873d6eeb0a7ab39553a13bb8a42
anakurten-sudo <ana.kurten@escola.pr.gov.br>
2026-09-16 13:45:00 -0300

    script.js  atualizado

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const botoesCurtir = document.querSelectorAll(".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtiu = false;
+    botaoCurtir.addEventListener("click" , curtir);
+    function curtir(){
+        const contador = botaoCurtir.querySelector("span");
+        if(curtiu === false){
+        contador.textContent++;
+        curtiu =  true;}
+        else{
+            contador.textContent--;
+            curtiu = false;
+        }
+    }
+});
+    
+
+
 
